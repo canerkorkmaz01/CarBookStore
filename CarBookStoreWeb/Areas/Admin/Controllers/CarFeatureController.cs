946d22166a6bf483856ed9d1a111ecc6f794fc38
Caner KORKMAZ <caner.korkmaz@hotmail.com>
2022-05-03 00:37:09 +0300

    Edit

--- CarBookStoreWeb/Areas/Admin/Controllers/CarFeatureController.cs
+++ CarBookStoreWeb/Areas/Admin/Controllers/CarFeatureController.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CarBookStoreWeb.Areas.Admin.Controllers
@@ -13,6 +15,7 @@ namespace CarBookStoreWeb.Areas.Admin.Controllers
     [Authorize(Roles = "Administrators,Staff")]
     public class CarFeatureController : Controller
     {
+        private const string entityName = "Araç Özelliği";
         private readonly AppDbContext context;
 
         public CarFeatureController(
@@ -22,10 +25,12 @@ namespace CarBookStoreWeb.Areas.Admin.Controllers
             this.context = context;
         }
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
-            
-            return View();
+
+            var model = await context.CarFeatures.ToListAsync();
+            DropdownFill();
+            return View(model);
         }
     
         public IActionResult Create()
@@ -35,11 +40,42 @@ namespace CarBookStoreWeb.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(CarFeature carFeature)
+        public async Task <IActionResult> Create(CarFeature carFeature)
         {
+            carFeature.DateCreated = DateTime.Now;
+            carFeature.Enabled = true;
+            carFeature.UserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            context.Entry(carFeature).State = EntityState.Added;
+            try
+            {
+                await context.SaveChangesAsync();
+                TempData["success"] = $"{entityName} ekleme işlemi başarıyla tamamlanmıştır";
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
 
+                TempData["error"] = $"{entityName} güncelleme işlemi aynı isimli bir kayıt olduğu için tamamlanamıyor.";
+                return View(carFeature);
+            }
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int id)
+        {
+            DropdownFill();
+            var features = await context.CarFeatures.FindAsync(id);
+            return View(features);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(CarFeature carFeature)
+        {
             return View();
         }
+
 
         public void DropdownFill()
         {
